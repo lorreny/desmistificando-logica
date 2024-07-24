f5509259d1079020c13c54cd0bd5dfb2e353dc09
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-07-24 18:56:54 -0300

    Create conta.py

---
+++ conta.py
@@ -0,0 +1,19 @@
+olho = 1
+pe = 2
+soma = olho + pe
+print(soma)
+print(type(soma))
+
+#string
+olhos = '1'
+pes = '2'
+somas = olhos + pes
+print(somas)
+print(type(somas))
+
+olhof = 1
+pef = 2
+inimigo = input('Informe seu nome, inimigo: ')
+print(inimigo)
+print(f'Olá {inimigo}, tudo de ruim pra você.')
+print(f'Olá {inimigo}, tudo de ruim pra você {inimigo}. {pes}')
