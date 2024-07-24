dbb38922921147fbd7f84a967b278567c0bf546f
Lorreny <38407983+lorreny@users.noreply.github.com>
2024-07-24 18:59:04 -0300

    Create desmistificandologica.py

---
+++ desmistificandologica.py
@@ -0,0 +1,21 @@
+olho = 1
+pe = 1.0
+bo = True
+palavra = 'Qualquer coisa'
+#palavra1 = input('Diga seu nome, madito: ')
+#numero = int(input('diga um numero: '))
+#decimal = float(input('diga um numero: '))
+
+#palavra2 = int(input('Diga seu nome, madito: '))
+
+print('Olá mundo')
+#pegar o tipo de dados
+print(type(olho))
+print(type(pe))
+print(type(bo))
+print(type(palavra))
+#print(type(numero))
+#print(type(decimal))
+
+#texto com quebra de linha
+print(f'{olho}, \n{pe}, \n{bo}, \n{palavra}')
